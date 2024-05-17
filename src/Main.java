c0a3d016a952062a1b413fc74d2f1d295454382d
Marcosrodriguez <147057536+Marcosrodriguez0000@users.noreply.github.com>
2024-05-17 08:20:44 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String csvFile = "C:\\Users\\Mañana\\Downloads\\lista_alumnos (1).csv"; // Ruta al archivo CSV
-        int numEstudiantes = 0; // Variable para almacenar el número de estudiantes
-        int sumaEdades = 0; // Variable para almacenar la suma de las edades
-        double sumaNotas = 0; // Variable para almacenar la suma de las notas
-        double notaMaxima = Double.MIN_VALUE; // Variable para almacenar la nota máxima, inicializada con el valor mínimo posible
-        double notaMinima = Double.MAX_VALUE; // Variable para almacenar la nota mínima, inicializada con el valor máximo posible
+        String csvFile = "C:\\Users\\Mañana\\Downloads\\lista_alumnos (1).csv";
+        int numEstudiantes = 0; 
+        int sumaEdades = 0; 
+        double sumaNotas = 0; 
+        double notaMaxima = Double.MIN_VALUE; 
+        double notaMinima = Double.MAX_VALUE; 
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             String line;
             // Leer cada línea del archivo
             while ((line = br.readLine()) != null) {
-                System.out.println(line); // Imprimir cada línea del archivo
-                String[] datos = line.split(","); // Dividir la línea en datos separados por comas
-                if (!datos[0].equals("nombre")) { // Omitir la primera línea que contiene encabezados
-                    numEstudiantes++; // Incrementar el contador de estudiantes
+                System.out.println(line);
+                String[] datos = line.split(","); 
+                if (!datos[0].equals("nombre")) { 
+                    numEstudiantes++; 
                 }
             }
         } catch (IOException e) {
